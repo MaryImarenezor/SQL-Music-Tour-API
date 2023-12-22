062affe4e4493e3f1d6473d664f50d9c2de2fb6d
Mary I <maryimarenezor13@gmail.com>
2023-12-21 19:34:23 -0600

    completed activity

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-// DEPENDENCIES
+// DEPENDENCIES - where we import all of our packages
+const { Sequelize } = require('sequelize')
 const express = require('express')
 const app = express()
 
-// CONFIGURATION / MIDDLEWARE
+
+// CONFIGURATION / MIDDLEWARE - configure the imported packages and put them to use
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate()
+    console.log(`SQL Database connected at ${process.env.PG_URI}! :D`)
+} catch (error) {
+    console.log(`unable to connect to ${process.env.PG_URI}... :(`)
+}
 
 // ROOT
 app.get('/', (req, res) => {
@@ -14,7 +24,7 @@ app.get('/', (req, res) => {
     })
 })
 
-// LISTEN
+// LISTEN - telling our app which port to listen to
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
