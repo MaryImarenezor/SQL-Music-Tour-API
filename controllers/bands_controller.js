269bde4d8cb20fabc38406e8437f34c5b226d94e
Mary I <maryimarenezor13@gmail.com>
2024-01-05 22:52:39 -0600

    added association queries

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -3,7 +3,7 @@
 // DEPENDENCIES
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db
+const { Band, MeetGreet, Event, SetTime } = db
 const { Op } = require('sequelize')
 
 // FIND ALL OF THE BANDS - The Index Route
@@ -20,11 +20,31 @@ bands.get('/', async (req, res) => {
     }
 })
 
-// FIND A SPECIFIC BAND USING AN ID - The Show Route
-bands.get('/:id', async (req, res) => {
+// FIND A SPECIFIC BAND USING A NAME - The Show Route
+bands.get('/:name', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
-            where: {band_id: req.params.id}
+            where: {name: req.params.name},
+            include: [
+            {
+                model: MeetGreet,
+                as: "meet-greet",
+                include: {
+                    model: Event, 
+                    as: "event",
+                    where: {name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%`}}
+                }
+            }, 
+            {
+                model: SetTime,
+                as: "set_times",
+                include: {
+                    model: Event,
+                    as: "event",
+                    where: {name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%`}}
+                }
+            }
+        ]
         })
         res.status(200).json(foundBand)
     } catch (error) {
